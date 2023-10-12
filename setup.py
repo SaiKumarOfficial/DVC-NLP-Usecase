39b0a0ff3f3130476df9845b9985e17f2a0e4259
saikumar <sai680513@gmail.com>
2023-10-12 17:01:23 +0530

    Update for src installation

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 ## edit below variables as per your requirements -
-REPO_NAME = "REPO_NAME"
-AUTHOR_USER_NAME = "c17hawke"
+REPO_NAME = "DVC-NLP-Usecase"
+AUTHOR_USER_NAME = "SaiKumarOfficial"
 SRC_REPO = "src"
 LIST_OF_REQUIREMENTS = []
 
@@ -14,13 +14,13 @@ setup(
     name=SRC_REPO,
     version="0.0.1",
     author=AUTHOR_USER_NAME,
-    description="A small package for DVC",
+    description="A small package for DVC-NLP Usecase",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    author_email="sunny.c17hawke@gmail.com",
+    author_email="sai680513@gmail.com",
     packages=[SRC_REPO],
-    license="MIT",
+    
     python_requires=">=3.6",
     install_requires=LIST_OF_REQUIREMENTS
 )
